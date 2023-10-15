07ce883c3cc7c4ff7523dbbdffd70ceb4e4c8074
Dev-Destructor <dev.debobratapal@gmail.com>
2023-10-15 22:17:16 +0530

    renamed directory name

---
+++ 3_tier_architecture/README.md
@@ -0,0 +1,52 @@
+## <center> VPC with 3 Tier Architecture </center>
+
+### Architecture Diagram
+
+![Architecture Diagram](./assets/3-tier-aws.svg)
+
+### Prerequisites
+
+> Terraform installed on your machine <br />
+> AWS CLI installed on your machine
+
+### Steps to follow
+
+1. Clone the repository
+
+2. Use
+
+   ```bash
+   aws configure
+   ```
+
+   to configure your aws credentials
+
+3. Use
+
+   ```bash
+   cd iaac-for-vpc-with-ec2
+   ```
+
+   to go to the directory
+
+4. Use
+
+   ```bash
+   terraform init
+   ```
+
+   to initialize the terraform
+
+5. Use
+
+   ```bash
+    terraform plan
+   ```
+
+   to see the changes that will be made
+
+6. Use
+   ```bash
+     terraform apply
+   ```
+   to apply the changes
---
+++ 3_tier_architecture/variables.tf
@@ -0,0 +1,81 @@
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+  default     = "destructor"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "key-pair" {
+  description = "AWS key pair"
+  type        = string
+  default     = "tf-key-pair.pem"
+}
+
+variable "instance" {
+  description = "AWS instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_map" {
+  type = map(string)
+  default = {
+    "eu-north-1"     = "ami-0989fb15ce71ba39e"
+    "eu-west-3"      = "ami-05b5a865c3579bbc4"
+    "eu-west-2"      = "ami-0eb260c4d5475b901"
+    "eu-west-1"      = "ami-01dd271720c1ba44f"
+    "ap-south-1"     = "ami-0f5ee92e2d63afc18"
+    "ap-northeast-3" = "ami-0da13880f921c96a5"
+    "ap-northeast-2" = "ami-0c9c942bd7bf113a2"
+    "ap-northeast-1" = "ami-0d52744d6551d851e"
+    "ca-central-1"   = "ami-0ea18256de20ecdfc"
+    "sa-east-1"      = "ami-0af6e9042ea5a4e3e"
+    "ap-southeast-1" = "ami-0df7a207adb9748c7"
+    "ap-southeast-2" = "ami-0310483fb2b488153"
+    "eu-central-1"   = "ami-04e601abe3e1a910f"
+    "us-east-1"      = "ami-053b0d53c279acc90"
+    "us-east-2"      = "ami-024e6efaf93d85776"
+    "us-west-1"      = "ami-0f8e81a3da6e2510a"
+    "us-west-2"      = "ami-03f65b8614a860c29"
+  }
+}
+
+variable "user_ip" {
+  description = "User IP"
+  default     = "0.0.0.0/0"
+}
+
+variable "db_port" {
+  description = "value of db port"
+  type        = number
+  default     = 3306
+}
+
+variable "public_instance_listening_port" {
+  description = "Public instance listening port"
+  type        = number
+  default     = 3000
+}
+
+variable "private_instance_listening_port" {
+  description = "Private instance listening port"
+  type        = number
+  default     = 9000
+}
+
+variable "auto-scale-max-size" {
+  description = "Auto scale max size"
+  type        = number
+  default     = 2
+}
+
+variable "auto-scale-min-size" {
+  description = "Auto scale min size"
+  type        = number
+  default     = 1
+}
