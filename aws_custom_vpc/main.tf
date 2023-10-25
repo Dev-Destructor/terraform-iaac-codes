29b31108303176f8097ff7a453098462f04d164d
Dev-Destructor <dev.debobratapal@gmail.com>
2023-10-25 21:03:12 +0530

    updated route table cidr configuration

--- aws_custom_vpc/main.tf
+++ aws_custom_vpc/main.tf
@@ -100,12 +100,12 @@ resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.prod-vpc.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block     = var.private_subnet_01_cidr
     nat_gateway_id = aws_nat_gateway.nat_gateway_subnet_1.id
   }
 
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block     = var.private_subnet_02_cidr
     nat_gateway_id = aws_nat_gateway.nat_gateway_subnet_2.id
   }
 
